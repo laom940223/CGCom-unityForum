e53f96e19061e08b195cb107202003fcf9473bb8
Manuel Lara <laom940223@gmail.com>
2021-03-15 12:57:28 -0600

    Add basci JSON API

---
+++ server/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var { notFound, errorHandler} = require('./middle')
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get("/", (req, res, next)=>{
+
+    res.json({
+
+        message: "Welcome to community API "
+
+    })
+})
+
+
+app.use(notFound)
+app.use(errorHandler)
+module.exports = app;
